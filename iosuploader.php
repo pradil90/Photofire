37def6f5f596a827e0799353a1964a2c7031706a
pradeep <pradil90@gmail.com>
2015-08-21 00:43:02 -0700

    IOS client upload
    Supports IOS client upload

---
+++ iosuploader.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$Username='Mavericks';
+// $Username=$_COOKIE["Username"];
+$target_path = './project/'.$Username.'/';
+echo $target_path;
+$randomNumber = rand(0, 99999);
+
+
+$target_path = $target_path .$randomNumber. basename( $_FILES['file']['name']); 
+$target=basename( $_FILES['file']['name']); 
+
+$int=move_uploaded_file($_FILES['file']['tmp_name'], $target_path);
+echo "The file  $target has been uploaded";
+
+
+
+?>
+
